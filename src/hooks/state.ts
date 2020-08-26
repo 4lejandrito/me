8d15925f0c7f4f5c9f5ba04e3da8c2b39ea4b395
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-08-26 21:33:14 +0200

    Registering dance and member events

--- src/hooks/state.ts
+++ src/hooks/state.ts
@@ -1,16 +1,23 @@
 import { createContainer } from 'unstated-next'
 import { useState } from 'react'
-import { family } from '../components/Family'
+import { family, FamilyMember } from '../components/Family'
+import analytics from '../util/analytics'
 
 const State = createContainer(() => {
   let [member, setMember] = useState(family[0])
   let [dancing, setDancing] = useState(false)
-  let toggleDancing = () => setDancing(!dancing)
+  let toggleDancing = () => {
+    if (!dancing) analytics('dance')
+    setDancing(!dancing)
+  }
   return {
     member: dancing ? family[0] : member,
     dancing,
     toggleDancing,
-    setMember
+    setMember: (member: FamilyMember) => {
+      analytics(member.name)
+      setMember(member)
+    }
   }
 })
 
